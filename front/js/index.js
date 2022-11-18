ac5d9fde921de30c0b5a9495d76d0b6089cde9ab
ngencin <niademe@gmail.com>
2022-11-18 18:10:26 +0100

    feature

--- front/js/index.js
+++ front/js/index.js
@@ -9,12 +9,38 @@
 
 async function getProducts(){
    const response = await fetch("http://localhost:3000/api/products")
-    console.log(response)
     const responseJson = await response.json()
-    console.log(responseJson)
-}
-getProducts().then()
+   
+   
+    // Array Json
+
+let valeur = responseJson[0].imageUrl // récupère valeur du array Json
 
 
 // Products
+
+// Récupération lien produit
+let createLink = document.createElement("a") // élement lien
+createLink.href = "./product.html?id=42" // adresse url
+createLink.text='Kanap'
+let link = document.querySelector('#items').appendChild(createLink) // element parent items , enfant de cet élement article
+
+
+// Récupération section article
+let createArticle = document.createElement("article") // création balise article
+let article = createLink.appendChild(createArticle) // balise article enfant de cardArticle
+
+
+// Récupération image
+let createImg = document.createElement("img") // création élément img 
+createImg.text = "Kanap Sinopé" 
+let img = createArticle.appendChild(createImg) 
+img.src = valeur    // appel de l'image via array json
+
+
+// Récupération h3
+let createH3 = document.createElement("h3")
+let h3 = createArticle.appendChild(createH3)
+}
+getProducts().then()
 
